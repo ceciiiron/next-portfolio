65a824f2555e64e46500d6ddcb08cab4dc444dde
Ceciron <cecironalejoiii@gmail.com>
2025-06-16 14:47:37 +0800

    Feat: Updated UI

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,43 +1,65 @@
 import { ProjectCard } from "@/components/cards/ProjectCard";
 import React from "react";
 import { Section } from "@/components/ui/section";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
-export default function Home() {
+export default function page() {
 	return (
-		<div className="max-w-6xl mx-auto relative mt-[12rem]">
-			<div className="flex flex-col justify-center gap-2 col-span-2">
-				<div>
-					<p className="inline-block mb-4  border px-6 py-2 rounded-md bg-white shadow-xs">Built with love and passion ✨</p>
-				</div>
+		<>
+			<div className="max-w-6xl mx-auto relative mt-[7rem]">
+				<Breadcrumb>
+					<BreadcrumbList>
+						<BreadcrumbItem>
+							<BreadcrumbLink href="/">Home</BreadcrumbLink>
+						</BreadcrumbItem>
+						<BreadcrumbSeparator />
+						<BreadcrumbItem>Projects</BreadcrumbItem>
+					</BreadcrumbList>
+				</Breadcrumb>
 
-				<h1 className="text-6xl font-extrabold leading-tight">
-					{" "}
-					<span className="">Projects</span>
-				</h1>
-				<p className="text-gray-400 text-4xl font-bold">
-					Turning complex problems <br /> into simple intuitive solutions <br /> that serve real people.
-				</p>
+				<div className="mt-[4rem]">
+					<div className="flex flex-col justify-center gap-2 col-span-2">
+						<div>
+							<p className="inline-block mb-4  border px-6 py-2 rounded-md bg-white shadow-xs">Built with love and passion ✨</p>
+						</div>
+
+						<h1 className="text-6xl font-extrabold leading-tight">
+							{" "}
+							<span className="">Projects</span>
+						</h1>
+						<p className="text-gray-400 text-4xl font-bold">
+							Turning complex problems <br /> into simple intuitive solutions <br /> that serve real people.
+						</p>
+					</div>
+
+					<Section>
+						<div className="cards-container grid grid-cols-3 gap-10">
+							<ProjectCard
+								url="/"
+								title="BRHMC Queuing System"
+								description="Bicol Regional Hospital and Medical Center - Electronic Queuing System"
+								imageSrc="/assets/images/projects/qs/header.png"
+							/>
+
+							<ProjectCard
+								url="/"
+								title="BUCS-RDMS"
+								description="Bicol University College of Science - Research and Document Management System"
+								imageSrc="/assets/images/projects/bucsrdms/header.png"
+							/>
+
+							<ProjectCard url="/" title="One Banwaan: A Barangay-Centric Smart Community in Legazpi City" imageSrc="/assets/images/projects/bucsrdms/1.jpeg" />
+
+							<ProjectCard
+								title="The Courier Company"
+								url="/projects/the-courier-company"
+								description={<p>Courier Management System</p>}
+								imageSrc="/assets/images/projects/bucsrdms/header.png"
+							/>
+						</div>
+					</Section>
+				</div>
 			</div>
-
-			<Section>
-				<div className="cards-container grid grid-cols-3 gap-10">
-					<ProjectCard
-						title="BRHMC Queuing System"
-						description="Bicol Regional Hospital and Medical Center - Electronic Queuing System"
-						imageSrc="/assets/images/projects/qs/header.png"
-					/>
-
-					<ProjectCard
-						title="BUCS-RDMS"
-						description="Bicol University College of Science - Research and Document Management System"
-						imageSrc="/assets/images/projects/bucsrdms/header.png"
-					/>
-
-					<ProjectCard title="One Banwaan: A Barangay-Centric Smart Community in Legazpi City" imageSrc="/assets/images/projects/bucsrdms/1.jpeg" />
-
-					<ProjectCard title="The Courier Company" description={<p>Courier Management System</p>} imageSrc="/assets/images/projects/bucsrdms/header.png" />
-				</div>
-			</Section>
-		</div>
+		</>
 	);
 }
--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -5,24 +5,27 @@ import React from "react";
 interface ProjectCardProps {
 	title: string;
 	description?: React.ReactNode;
+	url: string;
 	imageSrc: string;
 	imageAlt?: string;
 	className?: string;
 }
 
-export const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, imageSrc, imageAlt = "Project thumbnail", className = "" }) => {
+export const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, imageSrc, imageAlt = "Project thumbnail", className = "", url = "/" }) => {
 	return (
-		<Card className={`relative p-0 overflow-hidden gap-0 ${className}`}>
-			<div className="px-0 overflow-hidden ">
-				<div className="h-48 w-full overflow-hidden">
-					<Image src={imageSrc} width={300} height={500} className="object-cover object-fit object-center w-full overflow-hidden h-48" alt={imageAlt} />
+		<a href={url}>
+			<Card className={`relative p-0 overflow-hidden gap-0 ${className}`}>
+				<div className="px-0 overflow-hidden ">
+					<div className="h-48 w-full overflow-hidden">
+						<Image src={imageSrc} width={300} height={500} className="object-cover object-fit object-center w-full overflow-hidden h-48" alt={imageAlt} />
+					</div>
 				</div>
-			</div>
 
-			<CardHeader className="w-full py-6">
-				<CardTitle className="text-lg">{title}</CardTitle>
-				{description && <CardDescription>{description}</CardDescription>}
-			</CardHeader>
-		</Card>
+				<CardHeader className="w-full py-6">
+					<CardTitle className="text-lg">{title}</CardTitle>
+					{description && <CardDescription>{description}</CardDescription>}
+				</CardHeader>
+			</Card>
+		</a>
 	);
 };
